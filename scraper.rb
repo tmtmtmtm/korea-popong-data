59564484506bf3ad32c1b00121deadf2628e7b26
Tony Bowden <tony@tmtm.com>
2015-09-14 09:31:22 +0100

    Korea: fetch more data via Popong API

--- scraper.rb
+++ scraper.rb
@@ -2,16 +2,29 @@
 # encoding: utf-8
 
 require 'colorize'
+require 'csv'
+require 'json'
 require 'open-uri'
 require 'pry'
 require 'scraperwiki'
-require 'csv'
+
+require 'open-uri/cached'
+OpenURI::Cache.cache_path = '.cache'
+
+def json_from(url)
+  JSON.parse(open(url).read, symbolize_names: true)
+end
+
+@POPONG_API = 'http://api.popong.com/v0.1/person/%s?api_key=test'
 
 file = 'https://raw.githubusercontent.com/teampopong/data-for-rnd/master/assembly.csv'
 raw = open(file).read
 
 csv = CSV.parse(raw, headers: true, header_converters: :symbol)
 csv.each do |row|
+  json = json_from(@POPONG_API % row[:person_id]) rescue {}
+  warn "%s vs %s".red % [row[:name_en], json[:name_en]] unless row[:name_en] == json[:name_en]
+
   data = { 
     id: row[:person_id],
     identifier__popong: row[:person_id],
@@ -27,7 +40,14 @@ csv.each do |row|
     photo: 'http://www.assembly.go.kr/photo/%s.jpg)' % row[:photo_id],
     term: 19,
     source: "github.com/teampopong/data-for-rnd",
+
+    wikipedia: json[:wiki],
+    twitter: json[:twitter],
+    facebook: json[:facebook],
   }
+
+
+  # puts data
   ScraperWiki.save_sqlite([:id, :term], data)
 end
 
