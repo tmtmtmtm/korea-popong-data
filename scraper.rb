b1540106c2c111dc2edf50c161acc12ba4f33361
Tony Bowden <tony@tmtm.com>
2017-01-05 14:46:32 +0000

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'csv'
 require 'open-uri'
@@ -15,20 +16,20 @@ raw = open(file).read
 csv = CSV.parse(raw, headers: true, header_converters: :symbol)
 csv.each do |row|
   data = {
-    id: row[:name_en].downcase.tr(' ','-'),
-    name: row[:name_en].strip,
-    name__ko: row[:name_kr],
-    name__cn: row[:name_cn],
+    id:         row[:name_en].downcase.tr(' ', '-'),
+    name:       row[:name_en].strip,
+    name__ko:   row[:name_kr],
+    name__cn:   row[:name_cn],
     birth_date: row[:birth],
-    party: row[:party],
-    area: row[:district],
-    phone: row[:off_phone],
-    homepage: row[:homepage],
-    email: row[:email],
-    photo: row[:photo],
-    term: 20,
-    source: 'https://github.com/teampopong/data-assembly',
+    party:      row[:party],
+    area:       row[:district],
+    phone:      row[:off_phone],
+    homepage:   row[:homepage],
+    email:      row[:email],
+    photo:      row[:photo],
+    term:       20,
+    source:     'https://github.com/teampopong/data-assembly',
   }
 
-  ScraperWiki.save_sqlite([:id, :name, :term], data)
+  ScraperWiki.save_sqlite(%i(id name term), data)
 end
